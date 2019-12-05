5a1d944373ff5f72f78005ba453f6da8c777c6b5
WestonJN <westonnyirenda@gmail.com>
2019-12-05 10:26:06 +0200

    Function edits

--- function.js
+++ function.js
@@ -1,71 +1,61 @@
-var memArray= ["A","A","B","B","C","C","D","D","E","E","F","F"];
-var memValues=[];
-var memTileId=[];
-var tilesFlipped =0;
+const cards = document.querySelectorAll('.memBlock');
 
-Array.prototype.memTileShuffle = function(){
-    var i = this.length, j, temp;
-    while (--i > 0){
-        j= Math.floor(Math.random() * (i+1));
-        temp= this[j];
-        this[j]=this[i];
-        this[i]=temp;
-    }
+let flippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
+  this.classList.add('flip');
+
+  if (!flippedCard) {
+    flippedCard = true;
+    firstCard = this;
+
+    return;
+  }
+
+  secondCard = this;
+  checkForMatch();
 }
-function newBoard(){
-    tilesFlipped =0;
-    var output ='';
-    memArray.memTileShuffle();
-    for (var i = 0; i < memArray.length; i++){
-        output += '<div id="tile_'+i+'" onclick="memoryFlipTile(his,\''+memArray[i]+'\')"></div>';
-    }
-    document.getElementById('memBoard').innerHTML=output;
+
+function checkForMatch() {
+  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+
+  isMatch ? disableCards() : unflipCards();
 }
 
-function memoryFlipTile(tile,val){
-    if(tile.innerHTML == "" && memValues.length < 2){
-        tile.style.background == '#FFF';
-        tile.innerHTML = val;
-        if(memValues.length == 0){
-            memValues.push(val);
-            memTileId.push(tile.id);
-        }else if(memValues.length == 1){
-            memValues.push(val);
-            memTileId.push(tile.id);
-            if(memValues[0] == memValues[1]){
-                tilesFlipped += 2;
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
 
-                memValues = [];
-                memTileId = [];
-
-                if(tilesFlipped == memArray.length){
-                    alert("Board cleared... Generating new board");
-                    document.getElementById('memBoard').innerHTML = "";
-                    newBoard();
-                }
-            }else{
-                function flip2Back(){
-                    var tile_1 = document.getElementById(memTileId[0]);
-                    var tile_2 = document.getElementById(memTileId[1]);
-                    tile_1.style.background = 'url(car1.png) no-repeat';
-                    tile_1.innerHTML = "";
-                    tile_2.style.background = 'url(car1.png) no-repeat';
-                    tile_2.innerHTML = "";
-                    memValues = [];
-                    memTileId = [];
-                }
-                setTimeout(flip2Back, 700);
-                function flip2Back(){
-                    var tile_1 = document.getElementById(memTileId[0]);
-                    var tile_2 = document.getElementById(memTileId[1]);
-                    tile_1.style.background = 'url(car1.png) no-repeat';
-                    tile_1.innerHTML = "";
-                    tile_2.style.background = 'url(car1.png) no-repeat';
-                    tile_2.innerHTML = "";
-                    memValues = [];
-                    memTileId = [];
-                }
-            }
-        } 
-    }
+  resetBoard();
 }
+
+function unflipCards() {
+  lockBoard = true;
+
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+
+    resetBoard();
+  }, 500);
+}
+
+function resetBoard() {
+  [flippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+  cards.forEach(card => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
+})();
+
+cards.forEach(card => card.addEventListener('click', flipCard));
+
