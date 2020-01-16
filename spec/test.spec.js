4a9502474aa2d66ad47f0b5c813353bd70ed5e7b
WestonJN <westonnyirenda@gmail.com>
2020-01-16 16:22:59 +0200

    Spec changes

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -1,44 +1,28 @@
-describe('memory game tests',function () {
+describe('Memory game', function () {
+
+	const jsdom = require('jsdom'),
+		  html  = require('../spec/indexof'),
+		  script  = require('../src/script');
+          
+	beforeEach(() => {
+	   dom      = new jsdom.JSDOM(html);
+	   document = dom.window.document;
+	});
+
+	// onClick btn simulator
+	const onClick = card => {
+	   card.addEventListener('flipCard', e => {
+		  script.flipCard();
+	   });
+ 
+	   const e = new dom.window.Event("flipCard");
+ 
+	   card.dispatchEvent(e);
+	}
+
+	it('Should return true', function () {
+		const card = document.getElementsByClassName('memBlock');
+		expect(card.innerHTML).toBe('true');
+	});
 	
-	const html = require('./indexof.js')
-	const jsdom = require('jsdom')
-	const {JSDOM} = jsdom;
-
-	
-	
-
-	const clickSimulator = (arg)=>{
-		let event = new dom.window.MouseEvent('click', {
-			view: dom.window,
-			bubbles: true,
-			cancelable: false
-		})
-
-		let element = document.getElementsByTagName('div')[arg];
-		element.dispatchEvent(event);
-	};
-
-	beforeEach(()=>{
-
-		dom = new JSDOM(html,{
-        	
-        	runScripts: "dangerously",
-        	resources: "usable"
-      	});
-		document = dom.window.document;
-		game = require("../src/script.js");
-		
-	})
-
-	afterEach(()=>{
-		delete require.cache[require.resolve('../src/script.js')]
-	})
-
-	it("should be able to add addEventListener to all game tiles and make them clickable", ()=>{
-		clickSimulator(0) // specify which card index to click
-		expect(document.getElementsByClassName('flip').length).toEqual(0)
-		
-		clickSimulator(5) //flip another card at index 5
-		expect(document.getElementsByClassName('flip').length).toEqual(1)
-	})
-})
+});
